e1276d09355ed2cae64d308fd295ca2f44cd4c0e
Matt Bettinson <mattbettinson@me.com>
2014-02-09 18:34:07 -0500

    Initial commit
    Only have error trapping functionality at this point, and the GUI laid
    out.

---
+++ src/sleepcalculator/SleepCalculatorApp.java
@@ -0,0 +1,44 @@
+/*
+ * SleepCalculatorApp.java
+ */
+
+package sleepcalculator;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class SleepCalculatorApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new SleepCalculatorView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of SleepCalculatorApp
+     */
+    public static SleepCalculatorApp getApplication() {
+        return Application.getInstance(SleepCalculatorApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(SleepCalculatorApp.class, args);
+    }
+}
